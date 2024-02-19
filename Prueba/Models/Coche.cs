8585feba0103640601fb0aa756ad38dcf9346623
crisn <crisn@CHRIS>
2024-02-19 12:51:50 -0600

    Agregar archivos de proyecto.

---
+++ Prueba/Models/Coche.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prueba.Models
+{
+    [Table ("coches")]
+
+    public class Coche
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Coche_PK { get; set; }
+        public string Matricula { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Ano { get; set; }
+
+        public virtual Dueno dueno { get; set; }
+    }
+}
---
+++ Prueba/Models/Dueno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prueba.Models
+{
+    [Table("duenos")]
+    public class Dueno
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_duenos_PK { get; set; }
+        public string Nombre { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Fecha_nac { get; set; }
+        public string Direccion { get; set; }
+        public string Curp { get; set; }
+
+    }
+}
